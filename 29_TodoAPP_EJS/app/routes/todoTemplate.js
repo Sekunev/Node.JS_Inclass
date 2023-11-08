e5492e19aad44627a98f0edbad6d76e104f98ebd
sekune <ahlatlipost@gmail.com>
2023-11-08 21:08:33 +0300

    up

---
+++ 29_TodoAPP_EJS/app/routes/todoTemplate.js
@@ -0,0 +1,31 @@
+"use strict";
+/* -------------------------------------------------------
+    EXPRESSJS - TODO Project with Sequelize
+------------------------------------------------------- */
+const router = require("express").Router();
+
+// Call TODO Controller:
+const todoTemplate = require("../controllers/todoTemplate");
+
+// router.route('/')
+//     .get(todoTemplate.list) // LIST
+//     .post(todoTemplate.create) // CREATE
+
+// router.route('/:id')
+//     .get(todoTemplate.read) // READ
+//     .put(todoTemplate.update) // UPDATE
+//     .delete(todoTemplate.delete) // DELETE
+
+router.get("/", todoTemplate.list);
+
+router.get("/create", todoTemplate.create); // Form View
+router.post("/create", todoTemplate.create); // Form Processing
+
+router.get("/:id", todoTemplate.read);
+
+router.get("/:id/update", todoTemplate.update); // Form View
+router.post("/:id/update", todoTemplate.update); // Form Processing
+
+router.get("/:id/delete", todoTemplate.delete);
+
+module.exports = router;
